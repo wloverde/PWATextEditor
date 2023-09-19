e34f15d9be9965b4987bc2dee0eb6310a731ed62
Loverde <w.andrew.loverde@gmail.com>
2023-09-19 15:29:53 -0500

    updated Database variables for readability added NPM scripts

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,21 +15,20 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const db = await openDB('jate', 1);
-  const text = db.transaction('jate', 'readwrite');
-  const store = text.objectStore('jate');
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
   await store.put({ content }); // Assuming 'content' is an object you want to store
-  await text.done;
+  await tx.done;
   console.log('Data added to the database.');
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-
 export const getDb = async () => {
   const db = await openDB('jate', 1);
-  const text = db.transaction('jate', 'readonly');
-  const store = text.objectStore('jate');
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
   const data = await store.getAll();
-  await text.done;
+  await tx.done;
   return data;
 };
 
