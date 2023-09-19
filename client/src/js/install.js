95eb3b7be9a0d5aeb4ab688e2819aba04dd462f6
Loverde <w.andrew.loverde@gmail.com>
2023-09-19 15:46:23 -0500

    added CSS loader to allow testing

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,34 +1,39 @@
 const butInstall = document.getElementById('buttonInstall');
+let installPrompt;
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
-    const installPrompt = document.getElementById('install-prompt');
+  event.preventDefault();
+  installPrompt = event;
+  const installButton = document.getElementById('install-button');
+  installButton.style.display = 'block'; // Display your custom install button
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    if (installPrompt !== null) {
-      // Show the installation prompt to the user
-      installPrompt.prompt();
-  
-      // Wait for the user's response
-      const choiceResult = await installPrompt.userChoice;
-  
-      // Check if the user accepted or denied the installation
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the PWA installation.');
-      } else {
-        console.log('User declined the PWA installation.');
-      }
-  
-      // Reset the installPrompt variable
-      installPrompt = null;
-    }
-  });
+  if (installPrompt !== null) {
+    // Show the installation prompt to the user
+    installPrompt.prompt();
 
-// TODO: Add an handler for the `appinstalled` event
+    // Wait for the user's response
+    const choiceResult = await installPrompt.userChoice;
+
+    // Check if the user accepted or denied the installation
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the PWA installation.');
+    } else {
+      console.log('User declined the PWA installation.');
+    }
+
+    // Reset the installPrompt variable
+    installPrompt = null;
+  }
+});
+
+// Add an event handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    console.log('PWA was installed successfully.');
-  });
+  // Display an alert when the PWA is installed
+  alert('PWA was installed successfully.');
+  console.log('PWA was installed successfully.');
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Generate HTML files
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'], // Include the 'main' bundle in this HTML file
+      }),
       
+      // Configure the WebpackPwaManifest plugin
+      new WebpackPwaManifest({
+        name: 'Your PWA Name',
+        short_name: 'PWA Short Name',
+        description: 'Your PWA Description',
+        background_color: '#ffffff',
+        theme_color: '#31a9e1',
+        icons: [
+          {
+            src: path.resolve('src/images/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+      
+      // Configure the InjectManifest plugin for service workers
+      new InjectManifest({
+        swSrc: './src-sw.js', // Path to your service worker file
+        exclude: [/\.map$/, /manifest\.json$/], // Exclude specific files
+      }),
     ],
 
     module: {
       rules: [
-        
+        // CSS loaders for .css files
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
