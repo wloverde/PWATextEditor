e9dc7855771245e806f0f215c31a0ef7772b56b6
Loverde <w.andrew.loverde@gmail.com>
2023-09-19 15:37:53 -0500

    updated install.js for event handling

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,33 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    const installPrompt = document.getElementById('install-prompt');
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (installPrompt !== null) {
+      // Show the installation prompt to the user
+      installPrompt.prompt();
+  
+      // Wait for the user's response
+      const choiceResult = await installPrompt.userChoice;
+  
+      // Check if the user accepted or denied the installation
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the PWA installation.');
+      } else {
+        console.log('User declined the PWA installation.');
+      }
+  
+      // Reset the installPrompt variable
+      installPrompt = null;
+    }
+  });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('PWA was installed successfully.');
+  });
